6449ec9fc055050ff8f2a1da7ba483bc096079f1
Rock Young <rockforcareer@icloud.com>
2017-06-09 18:58:43 +0800

    Import project

---
+++ Project/LLRegex/LLRegex/LLRegex.h
@@ -0,0 +1,19 @@
+//
+//  LLRegex.h
+//  LLRegex
+//
+//  Created by Rock Young on 2017/6/9.
+//  Copyright © 2017年 Rock Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LLRegex.
+FOUNDATION_EXPORT double LLRegexVersionNumber;
+
+//! Project version string for LLRegex.
+FOUNDATION_EXPORT const unsigned char LLRegexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LLRegex/PublicHeader.h>
+
+
